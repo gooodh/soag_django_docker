ba3ec9caadfcb93f5b17181bd49de6ba5194a153
Nikulin <s.b.nikuliny@gmail.com>
2023-10-13 10:48:08 +0700

    stepik 1 test example

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-# from .views import HomePageView
 from .views import home_page_view, add_word, read_from_file
 
 
 urlpatterns = [
     # path('', HomePageView.as_view(), name='home'),
     path('', home_page_view, name='home'),
+    path('home/', home_page_view, name='home'),
     path('add_word/', add_word, name='add_word'),
     path('words_list/', read_from_file, name='words_list')
 ]
